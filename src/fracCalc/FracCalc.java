6db6e99eb38f9a7f1d98a5a8553755ca1475995e
WLXMATH <WLXMATH@WLX-B109-STU19.in.scu.k12.ca.us>
2018-10-26 09:08:34 -0700

    Submission for Checkpoint 1

--- src/fracCalc/FracCalc.java
+++ src/fracCalc/FracCalc.java
@@ -1,11 +1,13 @@
 package fracCalc;
-
+import java.util.*;
 public class FracCalc {
 
     public static void main(String[] args) 
     {
         // TODO: Read the input from the user and call produceAnswer with an equation
-
+    	Scanner fract = new Scanner(System.in);
+    	System.out.println("Enter the operation:");
+    	System.out.println(produceAnswer(fract.nextLine()));
     }
     
     // ** IMPORTANT ** DO NOT DELETE THIS FUNCTION.  This function will be used to test your code
@@ -16,11 +18,20 @@ public class FracCalc {
     //        
     // The function should return the result of the fraction after it has been calculated
     //      e.g. return ==> "1_1/4"
-    public static String produceAnswer(String input)
-    { 
+    public static String produceAnswer(String input) { 
         // TODO: Implement this function to produce the solution to the input
-        
-        return "";
+        String fract1 = "";
+        String fract2 = "";
+        String oper = "";
+    	for (int i = 0; i < input.indexOf(" "); i++) {
+        	fract1 = fract1 + input.charAt(i);
+        }
+
+    	oper = oper + (input.charAt(input.indexOf(" ") + 1));
+    	for (int i = input.indexOf(" ") + 3; i < input.length(); i++) {
+    		fract2 = fract2 + input.charAt(i);
+    	}
+        return fract2;
     }
 
     // TODO: Fill in the space below with any helper methods that you think you will need
